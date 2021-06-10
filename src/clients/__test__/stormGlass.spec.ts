5f6bc9af3d82ed0f3d0b9e32a6863882c96d805d
vinisaveg <viniisaveeg@gmail.com>
2021-06-10 19:52:19 -0300

    StormGlass Client init :zap:

--- src/clients/__test__/stormGlass.spec.ts
+++ src/clients/__test__/stormGlass.spec.ts
@@ -1,13 +1,22 @@
+import axios from "axios";
+
 import { StormGlass } from "@src/clients/stormGlass";
+
+import stormGlassWeather3HoursFixture from "@tests/fixtures/stormglass_weather_3_hours.json";
+import stormGlassNormalizedResponseWeather3HoursFixture from "@tests/fixtures/stormglass_normalized_response_weather_3_hours.json";
+
+jest.mock("axios");
 
 describe("StormGlass Client", () => {
   it("Should return the normalized forecast from StormGlass Service", async () => {
     const lat = -33.79234;
     const lng = 151.29453;
 
-    const stormGlass = new StormGlass();
+    axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture);
+
+    const stormGlass = new StormGlass(axios);
     const response = await stormGlass.fetchPoints(lat, lng);
 
-    expect(response).toEqual({});
+    expect(response).toEqual(stormGlassNormalizedResponseWeather3HoursFixture);
   });
 });
--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,5 +1,15 @@
+import { AxiosStatic } from "axios";
+
 export class StormGlass {
+  readonly stormGlassAPIParams =
+    "swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed";
+  readonly stormGlassAPISource = "noaaa";
+
+  constructor(protected request: AxiosStatic) {}
+
   public async fetchPoints(lat: number, lng: number): Promise<{}> {
-    return Promise.resolve({});
+    return this.request.get(
+      `https://api.stormglass.io/v2/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&lat=${lat}&lng=${lng}`
+    );
   }
 }
