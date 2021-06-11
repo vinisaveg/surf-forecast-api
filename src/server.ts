899d70c772185be83bcabe55b7499966601938c1
vinisaveg <viniisaveeg@gmail.com>
2021-06-11 15:27:30 -0300

    Adding connection to database :zap:

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,17 @@ import express, { Application } from "express";
 
 import { ForecastController } from "./controllers/forecast";
 
+import * as database from "./database";
+
 export class SetupServer extends Server {
   constructor(private port = 3000) {
     super();
   }
 
-  public init(): void {
+  public async init(): Promise<void> {
     this.setupExpress();
     this.setupControllers();
+    await this.databaseSetup();
   }
 
   private setupExpress(): void {
@@ -21,6 +24,14 @@ export class SetupServer extends Server {
     const forecastController = new ForecastController();
 
     this.addControllers([forecastController]);
+  }
+
+  private async databaseSetup(): Promise<void> {
+    await database.connect();
+  }
+
+  public async close(): Promise<void> {
+    await database.close();
   }
 
   public getApp(): Application {
