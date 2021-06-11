07e1be1d1c2c86ead129021c270380a24d9b2def
vinisaveg <viniisaveeg@gmail.com>
2021-06-11 17:13:13 -0300

    Adjusting beaches controller :wrench:

--- src/controllers/beaches.ts
+++ src/controllers/beaches.ts
@@ -1,10 +1,14 @@
 import { Controller, Post } from "@overnightjs/core";
+import { Beach } from "@src/models/beach";
 import { Request, Response } from "express";
 
 @Controller("beaches")
 export class BeachesController {
   @Post("")
   public async create(request: Request, response: Response): Promise<void> {
-    response.status(201).send({ ...request.body, id: "some-id" });
+    const beach = new Beach(request.body);
+    const result = await beach.save();
+
+    response.status(201).send(result);
   }
 }
