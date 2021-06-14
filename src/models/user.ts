621b3eb2869283213d77d80e588cb03ee8478c2d
vinisaveg <viniisaveeg@gmail.com>
2021-06-14 08:18:30 -0300

    Adding User model :zap:

---
+++ src/models/user.ts
@@ -0,0 +1,32 @@
+import mongoose, { Document, Model } from "mongoose";
+
+export interface User {
+  _id?: string;
+  name: string;
+  email: string;
+  password: string;
+}
+
+const schema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: {
+      type: String,
+      required: true,
+      unique: [true, "Email must be unique"],
+    },
+    password: { type: String, required: true },
+  },
+  {
+    toJSON: {
+      transform: (_, ret): void => {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+interface UserModel extends Omit<User, "_id">, Document {}
+export const User: Model<UserModel> = mongoose.model("User", schema);
