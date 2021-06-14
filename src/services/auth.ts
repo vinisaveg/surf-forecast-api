30e3ff4f9305b6fb90093b7afeaada5219320de2
vinisaveg <viniisaveeg@gmail.com>
2021-06-14 20:22:23 -0300

    Adding generateToken method :zap:

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,4 +1,6 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import config from "config";
 
 export default class AuthService {
   public static async hashPassword(
@@ -13,5 +15,11 @@ export default class AuthService {
     hashedPassword: string
   ): Promise<boolean> {
     return await bcrypt.compare(password, hashedPassword);
+  }
+
+  public static generateToken(payload: any): string {
+    return jwt.sign(payload, config.get("App.auth.key"), {
+      expiresIn: config.get("App.auth.tokenExpiresIn"),
+    });
   }
 }
