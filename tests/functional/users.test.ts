a86e28f01d0bab5f00703f9fc2c6f04c96d53884
vinisaveg <viniisaveeg@gmail.com>
2021-06-14 20:23:56 -0300

    Authenticate user tests :heavy_check_mark:

--- tests/functional/users.test.ts
+++ tests/functional/users.test.ts
@@ -57,4 +57,45 @@ describe("Users functional tests", () => {
       });
     });
   });
+
+  describe("When authenticating a user", () => {
+    it("Should generate a token for a valid user", async () => {
+      const newUser = {
+        name: "John Doe",
+        email: "john@mail.com",
+        password: "1234",
+      };
+
+      await new User(newUser).save();
+      const response = await global.testRequest
+        .post("/users/authenticate")
+        .send({ email: newUser.email, password: newUser.password });
+
+      expect(response.body).toEqual(
+        expect.objectContaining({ token: expect.any(String) })
+      );
+    });
+
+    it("Should return UNAUTHORIZED if the user was not found", async () => {
+      const response = await global.testRequest
+        .post("/users/authenticate")
+        .send({ email: "notjohn@mail.com", password: "1234" });
+
+      expect(response.status).toBe(401);
+    });
+
+    it("Should return ANAUTHORIZED if the user is found but the password does not match", async () => {
+      const newUser = {
+        name: "John Doe",
+        email: "john@mail.com",
+        password: "1234",
+      };
+      await new User(newUser).save();
+      const response = await global.testRequest
+        .post("/users/authenticate")
+        .send({ email: newUser.email, password: "different password" });
+
+      expect(response.status).toBe(401);
+    });
+  });
 });
