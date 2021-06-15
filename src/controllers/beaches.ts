b1b333578a588f6834794f394584d72cee1bc329
vinisaveg <viniisaveeg@gmail.com>
2021-06-14 21:26:42 -0300

    Adding authMiddleware :zap:

--- src/controllers/beaches.ts
+++ src/controllers/beaches.ts
@@ -1,14 +1,17 @@
-import { Controller, Post } from "@overnightjs/core";
-import { Beach } from "@src/models/beach";
+import { ClassMiddleware, Controller, Post } from "@overnightjs/core";
 import { Request, Response } from "express";
 import mongoose from "mongoose";
 
+import { Beach } from "@src/models/beach";
+import { authMiddleware } from "@src/middlewares/auth";
+
 @Controller("beaches")
+@ClassMiddleware(authMiddleware)
 export class BeachesController {
   @Post("")
   public async create(request: Request, response: Response): Promise<void> {
     try {
-      const beach = new Beach(request.body);
+      const beach = new Beach({ ...request.body, user: request.user?.id });
       const result = await beach.save();
 
       response.status(201).send(result);
