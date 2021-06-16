7d72a89ed866df513a517d42cfff1e562cf3a9e2
vinisaveg <viniisaveeg@gmail.com>
2021-06-16 19:13:35 -0300

    Adjusting tests :heavy_check_mark:

--- tests/functional/users.test.ts
+++ tests/functional/users.test.ts
@@ -37,7 +37,8 @@ describe("Users functional tests", () => {
       expect(response.status).toBe(422);
       expect(response.body).toEqual({
         code: 422,
-        error: "User validation failed: name: Path `name` is required.",
+        error: "Unprocessable Entity",
+        message: "User validation failed: name: Path `name` is required.",
       });
     });
 
@@ -53,7 +54,9 @@ describe("Users functional tests", () => {
       expect(response.status).toBe(409);
       expect(response.body).toEqual({
         code: 409,
-        error: "User validation failed: email: already exists in the database.",
+        error: "Conflict",
+        message:
+          "User validation failed: email: already exists in the database.",
       });
     });
   });
@@ -84,7 +87,7 @@ describe("Users functional tests", () => {
       expect(response.status).toBe(401);
     });
 
-    it("Should return ANAUTHORIZED if the user is found but the password does not match", async () => {
+    it("Should return UNAUTHORIZED if the user is found but the password does not match", async () => {
       const newUser = {
         name: "John Doe",
         email: "john@mail.com",
