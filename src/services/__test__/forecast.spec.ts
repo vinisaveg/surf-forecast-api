0322bec654fc7203139205929900d9076faef520
vinisaveg <viniisaveeg@gmail.com>
2021-06-17 19:05:52 -0300

    Adjusting forecast tests :heavy_check_mark:

--- src/services/__test__/forecast.spec.ts
+++ src/services/__test__/forecast.spec.ts
@@ -9,6 +9,94 @@ jest.mock("@src/clients/stormGlass");
 describe("Forecast Service", () => {
   const mockedStormGlassService = new StormGlass() as jest.Mocked<StormGlass>;
 
+  it("should return the forecast for mutiple beaches in the same hour with different ratings", async () => {
+    mockedStormGlassService.fetchPoints.mockResolvedValueOnce([
+      {
+        swellDirection: 123.41,
+        swellHeight: 0.21,
+        swellPeriod: 3.67,
+        time: "2020-04-26T00:00:00+00:00",
+        waveDirection: 232.12,
+        waveHeight: 0.46,
+        windDirection: 310.48,
+        windSpeed: 100,
+      },
+    ]);
+
+    mockedStormGlassService.fetchPoints.mockResolvedValueOnce([
+      {
+        swellDirection: 64.26,
+        swellHeight: 0.15,
+        swellPeriod: 13.89,
+        time: "2020-04-26T00:00:00+00:00",
+        waveDirection: 231.38,
+        waveHeight: 2.07,
+        windDirection: 299.45,
+        windSpeed: 100,
+      },
+    ]);
+
+    const beaches: Beach[] = [
+      {
+        lat: -33.792726,
+        lng: 151.289824,
+        name: "Manly",
+        position: GeoPosition.E,
+        user: "fake-id",
+      },
+      {
+        lat: -33.792726,
+        lng: 141.289824,
+        name: "Dee Why",
+        position: GeoPosition.S,
+        user: "fake-id",
+      },
+    ];
+
+    const expectedResponse = [
+      {
+        time: "2020-04-26T00:00:00+00:00",
+        forecast: [
+          {
+            lat: -33.792726,
+            lng: 151.289824,
+            name: "Manly",
+            position: "E",
+            rating: 2,
+            swellDirection: 123.41,
+            swellHeight: 0.21,
+            swellPeriod: 3.67,
+            time: "2020-04-26T00:00:00+00:00",
+            waveDirection: 232.12,
+            waveHeight: 0.46,
+            windDirection: 310.48,
+            windSpeed: 100,
+          },
+          {
+            lat: -33.792726,
+            lng: 141.289824,
+            name: "Dee Why",
+            position: "S",
+            rating: 3,
+            swellDirection: 64.26,
+            swellHeight: 0.15,
+            swellPeriod: 13.89,
+            time: "2020-04-26T00:00:00+00:00",
+            waveDirection: 231.38,
+            waveHeight: 2.07,
+            windDirection: 299.45,
+            windSpeed: 100,
+          },
+        ],
+      },
+    ];
+
+    const forecast = new Forecast(mockedStormGlassService);
+    const beachesWithRating = await forecast.processForecastForBeaches(beaches);
+
+    expect(beachesWithRating).toEqual(expectedResponse);
+  });
+
   it("Should return the forecast for a list of beaches", async () => {
     mockedStormGlassService.fetchPoints.mockResolvedValue(
       stormGlassNormalizedResponseWeather3HoursFixture
@@ -33,7 +121,7 @@ describe("Forecast Service", () => {
             lng: 151.289824,
             name: "Manly",
             position: "E",
-            rating: 1,
+            rating: 2,
             swellDirection: 64.26,
             swellHeight: 0.15,
             swellPeriod: 3.89,
@@ -53,7 +141,7 @@ describe("Forecast Service", () => {
             lng: 151.289824,
             name: "Manly",
             position: "E",
-            rating: 1,
+            rating: 2,
             swellDirection: 123.41,
             swellHeight: 0.21,
             swellPeriod: 3.67,
@@ -73,7 +161,7 @@ describe("Forecast Service", () => {
             lng: 151.289824,
             name: "Manly",
             position: "E",
-            rating: 1,
+            rating: 2,
             swellDirection: 182.56,
             swellHeight: 0.28,
             swellPeriod: 3.44,
